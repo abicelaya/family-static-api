c13a78cd05291770724e64e7281d6f62c3c0542b
abicelaya <abicelaya@gmail.com>
2022-10-09 11:46:27 +0000

    metodo de clase FamilyStructure

--- src/datastructures.py
+++ src/datastructures.py
@@ -11,26 +11,58 @@ from random import randint
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
         # example list of members
-        self._members = []
+        self._members = [{
+            "id": self._generateId(),
+            "first_name": "John",
+            "last_name": last_name,
+            "age": 33,
+            "lucky_numbers": [7, 13, 22]
+
+        }, {
+            "id": self._generateId(),
+            "first_name": "Jane",
+            "last_name": last_name,
+            "age": 35,
+            "lucky_numbers": [10, 14, 3]
+        }, {
+            "id": self._generateId(),
+            "first_name": "Jimmy",
+            "last_name": last_name,
+            "age": 5,
+            "lucky_numbers": [1]
+        }]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
+    # Use este método para generar ID de miembros aleatorios al agregar miembros a la lista
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
+        self._members.append(member)
         # fill this method and update the return
-        pass
+
 
     def delete_member(self, id):
+        for member_to_look in self._members:
+            if member_to_look.id == id:
+                self._members.remove(member_to_look)
+                
         # fill this method and update the return
-        pass
+        
 
     def get_member(self, id):
+        for member_to_look in self._members:
+            if member_to_look.id == id:
+                return member_to_look
+
         # fill this method and update the return
-        pass
+        
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+
+
+
